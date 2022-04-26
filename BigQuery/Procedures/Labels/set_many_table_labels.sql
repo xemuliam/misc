6f6d9f34bdc91822ad4acfe82c6169ec64db73c8
Slava Kalashnikov <xemuliam@users.noreply.github.com>
2022-04-26 17:18:33 +0300

    Update set_many_table_labels.sql

--- BigQuery/Procedures/Labels/set_many_table_labels.sql
+++ BigQuery/Procedures/Labels/set_many_table_labels.sql
@@ -1,27 +1,34 @@
-create or replace procedure set_many_table_labels(p_table_name STRING, p_labels_array ARRAY<STRUCT<name STRING, value STRING>>)
+create or replace procedure set_many_table_labels(
+  _in_table_name STRING,
+  _in_labels_array ARRAY<
+    STRUCT<
+      name STRING,
+      value STRING
+    >
+  >
+)
 begin
   declare labels array<struct<name string, value string>> default [];
 
-  call sdp_internal.get_all_table_labels(p_table_name, labels);
+  call get_all_table_labels(_in_table_name, labels);
 
   set labels = (
     select array(
       select as struct l.*
       from unnest(labels) l
       where name not in (
-        select name
-        from unnest(p_labels_array)
+        select name from unnest(_in_labels_array)
       )
-    ) || p_labels_array
+    ) || _in_labels_array
   );
 
   execute immediate """
-    alter table """||p_table_name||"""
+    alter table """||_in_table_name||"""
     SET OPTIONS (
-      labels=""" || (
-        select '[' || string_agg('STRUCT("'||l.name||'", "'||l.value||'")', ', ') || ']'
+      labels="""||(
+        select '['||string_agg('STRUCT("'||l.name||'", "'||l.value||'")', ', ')||']'
         from unnest(labels) l
-      ) || """
+      )||"""
     )"""
   ;
 end
