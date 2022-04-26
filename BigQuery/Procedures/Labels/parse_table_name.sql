bd15f8bf7e3250b5ee102ca1c437260cf15e2760
Slava Kalashnikov <xemuliam@users.noreply.github.com>
2022-04-26 16:06:49 +0300

    Create parse_table_name.sql

---
+++ BigQuery/Procedures/Labels/parse_table_name.sql
@@ -0,0 +1,15 @@
+create or replace procedure parse_table_name(p_table_name STRING, OUT p_table_struct STRUCT<dataset_name STRING, table_name STRING>)
+OPTIONS(
+  strict_mode=true)
+begin
+  declare dataset_name, table_name string default null;
+  declare names_array default (select array_reverse(split(replace(p_table_name, '`', ''), '.')));
+
+  set table_name = nullif(names_array[safe_ordinal(1)], '');
+  set dataset_name = ifnull(nullif(names_array[safe_ordinal(3)], '')||'.', '') || nullif(names_array[safe_ordinal(2)], '');
+
+  assert (table_name is not null and dataset_name is not null)
+    as 'Dataset or table name is missing';
+
+  set p_table_struct = (dataset_name, table_name);
+end
