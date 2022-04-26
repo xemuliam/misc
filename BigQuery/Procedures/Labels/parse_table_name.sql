19f559936b1344fcfe70f115898466d596ac618c
Slava Kalashnikov <xemuliam@users.noreply.github.com>
2022-04-26 17:02:23 +0300

    Update parse_table_name.sql

--- BigQuery/Procedures/Labels/parse_table_name.sql
+++ BigQuery/Procedures/Labels/parse_table_name.sql
@@ -1,7 +1,13 @@
-create or replace procedure parse_table_name(p_table_name STRING, OUT p_table_struct STRUCT<dataset_name STRING, table_name STRING>)
+create or replace procedure parse_in_table_name(
+  _in_table_name STRING,
+  OUT _out_table_struct STRUCT<
+    dataset_name STRING,
+    table_name STRING
+  >
+)
 begin
   declare dataset_name, table_name string default null;
-  declare names_array default (select array_reverse(split(replace(p_table_name, '`', ''), '.')));
+  declare names_array default (select array_reverse(split(replace(_in_table_name, '`', ''), '.')));
 
   set table_name = nullif(names_array[safe_ordinal(1)], '');
   set dataset_name = ifnull(nullif(names_array[safe_ordinal(3)], '')||'.', '') || nullif(names_array[safe_ordinal(2)], '');
@@ -9,5 +15,5 @@ begin
   assert (table_name is not null and dataset_name is not null)
     as 'Dataset or table name is missing';
 
-  set p_table_struct = (dataset_name, table_name);
+  set _out_table_struct = (dataset_name, table_name);
 end
