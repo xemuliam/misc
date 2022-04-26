6601180f80f8b9837dbba021c6dc94cc17092c7f
Slava Kalashnikov <xemuliam@users.noreply.github.com>
2022-04-26 16:02:40 +0300

    Create get_many_table_labels.sql

---
+++ BigQuery/Procedures/Labels/get_many_table_labels.sql
@@ -0,0 +1,19 @@
+create or replace procedure get_many_table_labels(p_table_name STRING, p_label_names_array ARRAY<STRING>, OUT p_labels_array ARRAY<STRUCT<name STRING, value STRING>>)
+OPTIONS(
+  strict_mode=true)
+begin
+  declare labels array<struct<name string, value string>> default [];
+
+  call sdp_internal.get_all_table_labels(p_table_name, labels);
+
+  set p_labels_array = (
+    select array(
+      select as struct l.*
+      from unnest(labels) l
+      where name in (
+        select n
+        from unnest(p_label_names_array) n
+      )
+    )
+  );
+end
