da4b84fb64c1acb6bc8864a18fbe68ac6815d5d4
Slava Kalashnikov <xemuliam@users.noreply.github.com>
2022-04-26 17:08:50 +0300

    Update get_many_table_labels.sql

--- BigQuery/Procedures/Labels/get_many_table_labels.sql
+++ BigQuery/Procedures/Labels/get_many_table_labels.sql
@@ -1,16 +1,24 @@
-create or replace procedure get_many_table_labels(p_table_name STRING, p_label_names_array ARRAY<STRING>, OUT p_labels_array ARRAY<STRUCT<name STRING, value STRING>>)
+create or replace procedure get_many_table_labels(
+  _in_table_name STRING,
+  _in_label_names_array ARRAY<STRING>,
+  OUT _out_labels_array ARRAY<
+    STRUCT<
+      name STRING,
+      value STRING
+    >
+  >
+)
 begin
   declare labels array<struct<name string, value string>> default [];
 
-  call sdp_internal.get_all_table_labels(p_table_name, labels);
+  call get_all_table_labels(_in_table_name, labels);
 
-  set p_labels_array = (
+  set _out_labels_array = (
     select array(
       select as struct l.*
       from unnest(labels) l
       where name in (
-        select n
-        from unnest(p_label_names_array) n
+        select n from unnest(_in_label_names_array) n
       )
     )
   );
