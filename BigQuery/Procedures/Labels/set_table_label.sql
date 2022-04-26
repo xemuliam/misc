7daad8187d525c5b35a8d7a4fa9cfcb736e69f4c
Slava Kalashnikov <xemuliam@users.noreply.github.com>
2022-04-26 17:20:36 +0300

    Update set_table_label.sql

--- BigQuery/Procedures/Labels/set_table_label.sql
+++ BigQuery/Procedures/Labels/set_table_label.sql
@@ -1,4 +1,8 @@
-create or replace procedure set_table_label(p_table_name STRING, p_label_name STRING, p_label_value STRING)
+create or replace procedure set_table_label(
+  _in_table_name STRING,
+  _in_label_name STRING,
+  _in_label_value STRING
+)
 begin
-  call sdp_internal.set_many_table_labels(p_table_name, [(p_label_name, p_label_value)]);
+  call set_many_table_labels(_in_table_name, [(_in_label_name, _in_label_value)]);
 end
