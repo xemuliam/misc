296df76844527056646f335a0e76f2626d054964
Slava Kalashnikov <xemuliam@users.noreply.github.com>
2022-05-05 08:32:26 +0300

    Update and rename set_table_label.sql to 40_set_table_label.sql

--- BigQuery/Procedures/Labels/40_set_table_label.sql
+++ BigQuery/Procedures/Labels/40_set_table_label.sql
@@ -1,8 +1,9 @@
-create or replace procedure set_table_label(
+create or replace procedure set_table_label (
   _in_table_name STRING,
   _in_label_name STRING,
   _in_label_value STRING
 )
+
 begin
   call set_many_table_labels(_in_table_name, [(_in_label_name, _in_label_value)]);
 end
