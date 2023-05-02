f48492070a1fcba6ace9b5b4fe64b87b8c6ce3e2
Slava Kalashnikov <xemuliam@users.noreply.github.com>
2023-05-02 18:06:42 +0300

    Update set_many_table_labels.sql

--- BigQuery/Labels/set_many_table_labels.sql
+++ BigQuery/Labels/set_many_table_labels.sql
@@ -3,6 +3,7 @@ options(
   strict_mode=false)
 begin
   declare labels array<struct<name string, value string>> default [];
+  declare table_struct STRUCT<dataset_name STRING, table_name STRING> default null;
 
   call get_all_table_labels(_in_table_name, labels);
 
@@ -14,16 +15,16 @@ begin
         select name
         from unnest(_in_labels_array)
       )
-    ) || _in_labels_array
+    )||_in_labels_array
   );
 
-  execute immediate """"""
-    alter table """"""||_in_table_name||""""""
-    set options (
-      labels=""""""||(
-        select '['||ifnull(string_agg('struct(""'||l.name||'"", ""'||l.value||'"")', ', '), '')||']'
+  call parse_table_name(_in_table_name, table_struct);
+  execute immediate """
+    alter table """||table_struct.dataset_name||'.'||table_struct.table_name||"""
+    SET OPTIONS (
+      labels="""||(
+        select '['||ifnull(string_agg('STRUCT("'||l.name||'", "'||l.value||'")', ', '), '')||']'
         from unnest(labels) l
-      )||""""""
-    )""""""
-  ;
+      )||"""
+    )""";
 end
