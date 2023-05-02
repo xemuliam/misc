0da0c2cdad8cdc88795123f7dd02a404708ecf0a
Slava Kalashnikov <xemuliam@users.noreply.github.com>
2023-05-02 11:35:20 +0300

    Delete BigQuery/Procedures/Labels directory

--- BigQuery/Procedures/Labels/40_set_table_label.sql
+++
@@ -1,9 +0,0 @@
-create or replace procedure set_table_label (
-  _in_table_name string,
-  _in_label_name string,
-  _in_label_value string
-)
-
-begin
-  call set_many_table_labels(_in_table_name, [(_in_label_name, _in_label_value)]);
-end
--- BigQuery/Procedures/Labels/50_remove_table_label.sql
+++
@@ -1,8 +0,0 @@
-create or replace procedure remove_table_label (
-  _in_table_name string,
-  _in_label_name string
-)
-
-begin
-  call set_table_label(_in_table_name, _in_label_name, cast(null as string));
-end
