c95395d94773493e516a2d52d072005c9ea9ae79
Slava Kalashnikov <xemuliam@users.noreply.github.com>
2023-05-02 14:12:34 +0300

    Update parse_and_validate_table_name.sql

--- BigQuery/Labels/parse_and_validate_table_name.sql
+++ BigQuery/Labels/parse_and_validate_table_name.sql
@@ -3,20 +3,16 @@ options(
   strict_mode=false)
 begin
   declare names_array default (select array_reverse(split(replace(_in_table_name, '`', ''), '.')));
-  declare is_valid default false;
 
-  execute immediate """"""
-    select exists(
-      select 1
-      from `""""""||ifnull(names_array[safe_ordinal(3)], @@project_id)||'.'||names_array[safe_ordinal(2)]||"""""".INFORMATION_SCHEMA.TABLES`
-      where table_name = '""""""||names_array[safe_ordinal(1)]||""""""'
-    )
-  """""" into is_valid;
+  execute immediate """
+    select (project_id||'.'||dataset_id, table_id)
+    from """||
+      ifnull('`'||names_array[safe_ordinal(3)]||'`.'||names_array[safe_ordinal(2)], ifnull('`'||@@project_id||'`.'||names_array[safe_ordinal(2)], '_SESSION'))||""".__TABLES__
+    where table_id = '"""||names_array[safe_ordinal(1)]||"""'
+  """ into _out_table_struct;
 
-  execute immediate """"""
+  execute immediate """
     assert @cond as
-      'table \""""""""||_in_table_name||""\"" not found'""
-  using is_valid as cond;
-
-  set _out_table_struct = (ifnull(names_array[safe_ordinal(3)], @@project_id)||'.'||names_array[safe_ordinal(2)], names_array[safe_ordinal(1)]);
+      'Table \""""||_in_table_name||"\" not found'"
+  using _out_table_struct.dataset_name||_out_table_struct.table_name is not null as cond;
 end
