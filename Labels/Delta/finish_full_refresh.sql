34003bd6cf084cf7148485bf8160075561159c9c
Slava Kalashnikov <xemuliam@users.noreply.github.com>
2023-05-02 11:37:47 +0300

    Create finish_full_refresh.sql

---
+++ Labels/Delta/finish_full_refresh.sql
@@ -0,0 +1,10 @@
+create procedure finish_full_refresh(_in_table_name string)
+options(
+  strict_mode=false)
+begin
+  call set_table_label(
+    _in_table_name,
+    'full-refresh',
+    format_timestamp('%y-%m-%d__%h-%m-%s', current_timestamp)
+  );
+end
