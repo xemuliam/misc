2cf49e876d7076619ceddcf9fdc34014cc45ab8f
Slava Kalashnikov <xemuliam@users.noreply.github.com>
2023-05-04 11:40:00 +0300

    Update get_delta_partitions_filter_src.sql

--- BigQuery/Delta/get_delta_partitions_filter_src.sql
+++ BigQuery/Delta/get_delta_partitions_filter_src.sql
@@ -1,8 +1,22 @@
-create procedure get_delta_partitions_filter_src(_in_tgt struct<table_name string, event_timestamp_column_name string>, _in_src struct<table_name string, event_timestamp_column_name string, additional_filter_condition string>, out _out_partitions_struct struct<max_tgt_ts timestamp, dates array<date>>)
-options(
-  strict_mode=false)
+create procedure get_delta_date_partitions_filter_src(
+  _in_tgt struct<
+    table_name string,
+    event_timestamp_column_name string
+  >,
+  _in_src struct<
+    table_name string,
+    event_timestamp_column_name string,
+    additional_filter_condition string
+  >,
+  
+  out _out_partitions_struct struct<
+    max_tgt_ts timestamp,
+    dates array<date>
+  >
+)
+options(strict_mode=false)
 begin
-  call get_delta_partitions_ext(
+  call get_delta_date_partitions_ext(
     (_in_tgt.table_name, _in_tgt.event_timestamp_column_name, null, null, null),
     _in_src,
     _out_partitions_struct
