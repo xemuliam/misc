92c74b77c0aeb64e78fcc4a9cfa5e8d09968ca10
Slava Kalashnikov <xemuliam@users.noreply.github.com>
2023-05-04 12:03:24 +0300

    Update and rename get_delta_partitions_metadata.sql to get_delta_date_partitions_metadata.sql

--- BigQuery/Delta/get_delta_date_partitions_metadata.sql
+++ BigQuery/Delta/get_delta_date_partitions_metadata.sql
@@ -1,10 +1,16 @@
-create procedure get_delta_partitions_metadata(_in_table_name string, out _out_delta_metadata_struct struct<last_job_start timestamp, max_tgt_ts timestamp>)
-options(
-  strict_mode=false)
+create procedure get_delta_date_partitions_metadata(
+  _in_table_name string,
+  
+  out _out_delta_metadata_struct struct<
+    last_job_start timestamp,
+    max_tgt_ts timestamp
+  >
+)
+options(strict_mode=false)
 begin
   declare delta_metadata_struct struct<last_job_start struct<name string, value timestamp>, max_tgt_ts struct<name string, value timestamp>>;
 
-  call get_delta_partitions_metadata_custom_labels(
+  call get_delta_date_partitions_metadata_custom_labels(
     _in_table_name,
     'last-job-start',
     'max-tgt-ts',
