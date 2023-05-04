867c01d4167364824390329b4583ee4885edd87c
Slava Kalashnikov <xemuliam@users.noreply.github.com>
2023-05-04 12:16:03 +0300

    Update set_delta_partitions_metadata_ext.sql

--- BigQuery/Delta/set_delta_partitions_metadata_ext.sql
+++ BigQuery/Delta/set_delta_partitions_metadata_ext.sql
@@ -1,6 +1,21 @@
-create procedure set_delta_partitions_metadata_ext(_in_table_name string, _in_delta_metadata_struct struct<last_job_start array<struct<name string, value timestamp>>, max_tgt_ts array<struct<name string, value timestamp>>>)
-options(
-  strict_mode=false)
+create procedure set_delta_partitions_metadata_ext(
+  _in_table_name string,
+  _in_delta_metadata_struct struct<
+    last_job_start array<
+      struct<
+        name string,
+        value timestamp
+      >
+    >,
+    max_tgt_ts array<
+      struct<
+        name string,
+        value timestamp
+      >
+    >
+  >
+)
+options(strict_mode=false)
 begin
   call set_many_table_labels(
     _in_table_name,
