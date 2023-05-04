de4c82f3cac6051f66655ccd517ed358d9258221
Slava Kalashnikov <xemuliam@users.noreply.github.com>
2023-05-04 11:22:50 +0300

    Update set_table_label.sql

--- BigQuery/Labels/set_table_label.sql
+++ BigQuery/Labels/set_table_label.sql
@@ -1,6 +1,9 @@
-create procedure set_table_label(_in_table_name string, _in_label_name string, _in_label_value string)
-options(
-  strict_mode=false)
+create procedure set_table_label(
+  _in_table_name string,
+  _in_label_name string,
+  _in_label_value string
+)
+options(strict_mode=false)
 begin
   call set_many_table_labels(_in_table_name, [(_in_label_name, _in_label_value)]);
 end
