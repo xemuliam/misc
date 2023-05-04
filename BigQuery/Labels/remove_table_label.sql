cb20fc8c0640062eaed58214796ddf8f6f911a61
Slava Kalashnikov <xemuliam@users.noreply.github.com>
2023-05-04 11:21:33 +0300

    Update remove_table_label.sql

--- BigQuery/Labels/remove_table_label.sql
+++ BigQuery/Labels/remove_table_label.sql
@@ -1,6 +1,8 @@
-create procedure remove_table_label(_in_table_name string, _in_label_name string)
-options(
-  strict_mode=false)
+create procedure remove_table_label(
+  _in_table_name string,
+  _in_label_name string
+)
+options(strict_mode=false)
 begin
   call set_table_label(_in_table_name, _in_label_name, cast(null as string));
 end
