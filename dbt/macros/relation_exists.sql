73cbf43214ece9c0c4d441ea85f2080a10c33992
Slava Kalashnikov <xemuliam@users.noreply.github.com>
2023-10-28 10:17:55 +0300

    change macro name

---
+++ dbt/macros/relation_exists.sql
@@ -0,0 +1,11 @@
+{% macro relation_exists(relation) %}
+
+  {%- set source_relation = adapter.get_relation(
+    database=relation.database,
+    schema=relation.schema,
+    identifier=relation.name)
+  -%}
+
+  {{ return(true if source_relation is not none else false) }}
+
+{% endmacro %}
