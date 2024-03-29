05770df3169332e07e9e5a87dec0f0ad2679f886
Slava Kalashnikov <xemuliam@users.noreply.github.com>
2024-03-29 15:55:33 +0200

    Update script.sql

--- dbt/macros/materializations/script.sql
+++ dbt/macros/materializations/script.sql
@@ -1,8 +1,14 @@
 {%- materialization script, default %}
+
+  {# Setup #}
   {{ run_hooks(pre_hooks) }}
+
   {%- call statement('main') %}
     {{ sql }}
   {%- endcall %}
+
+  {# Clean up #}
   {{ run_hooks(post_hooks) }}
+
   {{ return({'relations': []}) }}
 {%- endmaterialization %}
