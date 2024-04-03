2e07b0d74a5d6f309c139dbe905d36b143a34d35
Slava Kalashnikov <xemuliam@users.noreply.github.com>
2024-04-03 11:11:32 +0300

    Update copy.sql

--- dbt/macros/materializations/copy.sql
+++ dbt/macros/materializations/copy.sql
@@ -34,10 +34,10 @@
   {% endfor %}
 
   {# Check whether all realetions are tables #}
-  {%- if not_table_rel_array | length > 0 -%}
+  {% if not_table_rel_array | length > 0 %}
     {{ exceptions.raise_compiler_error('Only tables are allowed. Please revise following relation(s): ' ~
       not_table_rel_array | join(', ')) }}
-  {%- endif -%}
+  {% endif %}
 
   {# Create all relations list starting from first relation in model SQL #}
   {% set relations_array = [] %}
@@ -45,16 +45,16 @@
       if 0 <= model.raw_code.find('ref(') <= model.raw_code.find('source(')
       else src_array + ref_array %}
 
-  {# Check whether we have at least one realtion to work with #}
-  {%- if relations_array | length == 0 -%}
+  {# Check whether we have at least one relation to work with #}
+  {% if relations_array | length == 0 %}
     {{ exceptions.raise_compiler_error('No relation found in model SQL to copy data from') }}
-  {%- endif -%}
+  {% endif %}
 
   {# ========== Compatibility checks ========== #}
 
   {% set ns = namespace(is_matched_with_any_group=false) %}
 
-  {# Determine relations groups list. Each group will be performed as single copy job #}
+  {# Determine relations groups list. Each group will be processed then as single copy job #}
   {% set rel_grp_list = [[relations_array[0]]] %}
 
   {# Determine columns groups list. Contains unique columns lists across all model relations #}
@@ -66,18 +66,18 @@
   {# Determine columns list for target table #}
   {% set target_col_list = [] %}
   {% for col in adapter.get_columns_in_relation(destination if incremental_existing_target else relations_array[0]) %}
+    {# Determine flattened (to cover nested columns) table structure #}
     {{ target_col_list.extend(col.flatten() | map('string') | list) }}
   {% endfor %}
 
   {# Loop over all relations to make compatibility checks and distribute relations by groups #}
   {% for rel in relations_array[0 if incremental_existing_target else 1:] %}
-    {# Determine flattened (to cover nested columns) table structure from relation need to be checked #}
     {% set comp_col_list=[] %}
     {% for col in adapter.get_columns_in_relation(rel) %}
       {{ comp_col_list.extend(col.flatten() | map('string') | list) }}
     {% endfor %}
 
-    {# Compare realtion structure with target and raise exception if it doesn't fit #}
+    {# Relation structure must be subset of target structure to let copy works #}
     {% for col in comp_col_list %}
       {% if not col in target_col_list %}
         {{ exceptions.raise_compiler_error("Incompatible tables structure. " ~ col ~ " from  " ~ rel ~
@@ -88,15 +88,14 @@
     {# Maintain relations groups  #}
     {% set rel_index =  loop.index - (1 if incremental_existing_target else 0) %}
     
-    {# Skip first realtion because it's alredy added into relations and columns groups #}
+    {# Skip first relation because it's alredy added into relations and columns groups #}
     {% if rel_index > 0 %}
       {% set ns.is_matched_with_any_group = false %}
 
-      {# Compare relation's structure with each element in unique column groups #}
+      {# Compare relation structure with each column group #}
       {% for col_grp in col_grp_list %}
-        {# Check if relation structure exists in a group #}
         {% if comp_col_list | sort == col_grp | sort %}
-          {# Check if realtion doen't exist in relation group and add into group or raise exception#}
+          {# Check if relation exists in relation group #}
           {% if not relations_array[rel_index] in rel_grp_list[loop.index - 1] %}
             {{ rel_grp_list[loop.index - 1].extend([relations_array[rel_index]]) }}
             {% set ns.is_matched_with_any_group = true %}
@@ -107,7 +106,7 @@
         {% endif %}
       {% endfor %}
 
-      {# Maintain new column group and relation group if relation stucture doesn't match with existing groups  #}
+      {# Add new columns group and relation #}
       {% if not ns.is_matched_with_any_group %}
         {{ col_grp_list.append(comp_col_list) }}
         {{ rel_grp_list.append([relations_array[rel_index]]) }}
@@ -117,16 +116,16 @@
 
   {# ========== Working with database ========== #}
 
-  {# Determine whether we can use singlr copy statement for all relations #}
+  {# Determine whether we can have single copy statement for all relations #}
   {% set single_copy = true if rel_grp_list | length == 1 else false %}
 
   {% if not single_copy %}
     {# Create relation for interim table to put information there sequentially #}
-    {%- set interim_dest = api.Relation.create(
+    {% set interim_dest = api.Relation.create(
         database=config.get('interim_database', default = this.database),
         schema=config.get('interim_schema', default = this.schema),
         identifier=this.name + '__dbt_tmp_' + local_md5(modules.datetime.datetime.now() | string)
-    ) -%}
+    ) %}
 
     {# Create empty interim table using appropriate table tructure and metadata #}
     {%- call statement('main') %}
@@ -138,15 +137,15 @@
 
   {# Call adapter copy_table function for each relation group #}
   {% for rel_grp in rel_grp_list %}
-    {%- set result_str = adapter.copy_table(rel_grp, destination if single_copy else interim_dest,
-      copy_materialization if single_copy else 'incremental') -%}
+    {% set result_str = adapter.copy_table(rel_grp, destination if single_copy else interim_dest,
+      copy_materialization if single_copy else 'incremental') %}
     {{ store_result('main', response=result_str) }}
   {% endfor %}
 
   {% if not single_copy %}
     {# Call adapter copy_table function to put information into destination using interim table as source #}
-    {%- set result_str = adapter.copy_table(
-      interim_dest, destination, copy_materialization) -%}
+    {% set result_str = adapter.copy_table(
+      interim_dest, destination, copy_materialization) %}
     {{ store_result('main', response=result_str) }}
 
     {# Drop interim table #}
