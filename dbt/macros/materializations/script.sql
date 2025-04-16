d5622e152bc8c765f89e3cd641a90a0b3d85274d
Slava Kalashnikov <xemuliam@users.noreply.github.com>
2025-04-16 12:56:05 +0300

    Add bulk option to script materialization

--- dbt/macros/materializations/script.sql
+++ dbt/macros/materializations/script.sql
@@ -2,13 +2,24 @@
 
   {# Setup #}
   {{ run_hooks(pre_hooks) }}
-
-  {%- call statement('main') %}
-    {{ sql }}
-  {%- endcall %}
-
+  
+  {%- if config.get('bulk', default=False) -%}
+    {%- for query in sql.split(';') -%}
+      {%- if query | trim | length > 0 -%}
+        {% call statement('main') %}
+          {{ query }}
+        {% endcall %}
+      {%- endif -%}
+    {%- endfor -%}
+  {%- else -%}
+    {% call statement('main') %}
+      {{ sql }}
+    {% endcall %}
+  {%- endif -%}
+  
   {# Clean up #}
   {{ run_hooks(post_hooks) }}
 
   {{ return({'relations': []}) }}
+
 {%- endmaterialization %}
